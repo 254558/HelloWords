f46bc3ec388d7188b73e9d7a2f3a073d2a6514fc
张帅 <18141535210@163.com>
2025-06-12 00:38:42 +0800

    给词库打乱算法加了注释

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,8 +1,22 @@
+/* 
+洗牌算法
+你有10张扑克
+随便选一张牌和第一张牌交换顺序
+随便选一张牌和第二张牌交换顺序
+高端的算法往往采用原始的办法
+ */
+
+
 export function shuffle(array) {
-  let arr = array.slice()
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[arr[i], arr[j]] = [arr[j], arr[i]]
+
+  for (let i = array.length - 1; i > 0; i--) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+
   }
-  return arr
+
+
+  return array; 
 }
